55d10ba328d16b4028612f881c2b8b381a8d5867
gongpeng <ligongpeng123>
2022-03-29 18:50:20 +0800

    amend start 1 追加

--- src/com/boot/sample/Main.java
+++ src/com/boot/sample/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("args = " + args);
+        System.out.println("args1 = " + args);
     }
 }
